3c5ee1d841979aac6375ae8ec9dd9d75c6e9e297
Doug Chestnut <dougchestnut@gmail.com>
2015-04-09 11:20:20 -0400

    simple test of catalog search results

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -5,7 +5,7 @@ from google.appengine.ext import ndb
 from google.appengine.ext import testbed
 from google.appengine.datastore import datastore_stub_util
 
-class DemoTestCase(unittest.TestCase):
+class TestCases(unittest.TestCase):
 
   def setUp(self):
     tb = testbed.Testbed()
@@ -17,39 +17,13 @@ class DemoTestCase(unittest.TestCase):
   def tearDown(self):
     self.testbed.deactivate()
 
-  def testEventuallyConsistentGlobalQueryResult(self):
+  def testCatalog(self):
     app = endpoints.api_server([uvalibrary_api.CatalogApi], restricted=False)
     testapp = webtest.TestApp(app)
-    msg = {'query':''} # a dict representing the message object expected by insert
-#             # To be serialised to JSON by webtest
+
+    # Empty catalog search query (everything)
+    msg = {'query':''}
     resp = testapp.post_json('/_ah/spi/CatalogApi.search', msg)
-#    resp = testapp.get('/_ah/spi/uvalibrary/v0.1/catalog/search')
-    self.assertEqual(resp.json, {'expected': 'json response msg as dict'})
-
-#if __name__ == '__main__':
-#    unittest.main()
-
-
-#
-#
-#from google.appengine.ext import testbed
-#import webtest
-## ...
-#def setUp(self):
-#    tb = testbed.Testbed()
-#    tb.setup_env(current_version_id='testbed.version') #needed because endpoints expects a . in this value
-#    tb.activate()
-#    tb.init_all_stubs()
-#    self.testbed = tb
-#
-#def tearDown(self):
-#    self.testbed.deactivate()
-#
-#def test_endpoint_insert(self):
-#    app = endpoints.api_server([TestEndpoint], restricted=False)
-#    testapp = webtest.TestApp(app)
-#    msg = {...} # a dict representing the message object expected by insert
-#                # To be serialised to JSON by webtest
-#    resp = testapp.post_json('/_ah/spi/TestEndpoint.insert', msg)
-#
-#    self.assertEqual(resp.json, {'expected': 'json response msg as dict'})
+    # Make sure we have some results
+    self.assertIsNotNone( resp.json.get('count', None) )
+    self.assertTrue( resp.json['count'] > 0 )
